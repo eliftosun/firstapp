63b5ae38414ee6345e28cc7a33940c21b44b039a
Elif Tosun <elif@grabcad.com>
2013-05-09 22:08:54 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '06ed8c9d3ed7871cad708654c6f817c5736b4948d8ea13103a9502aa53ca7105cc426379df584d03eb9b578a98a6139eca7347ce42cba7f6e9822672ee3803e7'
